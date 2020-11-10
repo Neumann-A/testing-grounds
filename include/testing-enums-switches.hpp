42390defe77b3f474f261b4326f29fda315569fc
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-11-10 22:43:12 +0100

    code moved to mycel

--- include/testing-enums-switches.hpp
+++ include/testing-enums-switches.hpp
@@ -17,196 +17,18 @@
 #pragma message("HASCXX20")
 #endif
 //https://www.youtube.com/watch?v=INn3xa4pMfg
-template <typename Key, typename Value, std::size_t Size>
-struct static_map : std::array<std::pair<Key, Value>, Size>
-{
-    using type       = static_map<Key, Value, Size>;
-    using array_type = std::array<std::pair<Key, Value>, Size>;
-    [[nodiscard]] constexpr std::optional<Value> at(const Key &key) const noexcept
-    {
-        const auto it =
-            std::find_if(std::begin(*this), std::end(*this), [&key](const auto &val) { return val.first == key; });
-        if (it != std::end(*this))
-            return it->second;
-        return std::nullopt;
-    };
-    [[nodiscard]] constexpr Value operator[](const Key &key) const
-    {
-        const auto it =
-            std::find_if(std::begin(*this), std::end(*this), [&key](const auto &val) { return val.first == key; });
-        if (it != std::end(*this))
-            return it->second;
-        else
-            throw std::range_error("Key not found");
-    };
-    [[nodiscard]] constexpr Value operator[](const Value &val) const
-    {
-        const auto it =
-            std::find_if(std::begin(*this), std::end(*this), [&val](const auto &key) { return key.second == val; });
-        if (it != std::end(*this))
-            return it->first;
-        else
-            throw std::range_error("Value not found");
-    };
-    [[nodiscard]] constexpr static_map<Value, Key, Size> switch_key_value() const noexcept
-    {
-        std::array<std::pair<Value, Key>, Size> ret;
-        for (std::size_t i = 0; i < this->size(); ++i) {
-            ret[i] = std::make_pair(Value{static_cast<array_type>(*this)[i].second},
-                                    Key{static_cast<array_type>(*this)[i].first});
-        }
-        return static_map<Value, Key, Size>{ret};
-    }
-};
 
-template <typename EnumType, EnumType... EnumVals>
-struct enum_tuple
-{
-    using enum_type = EnumType;
-    static_assert(std::is_enum_v<EnumType>);
-    static constexpr auto count = sizeof...(EnumVals);
+#include <MyCEL/basics/templatehelpers.h>
+#include <MyCEL/basics/enumhelpers.h>
+#include <MyCEL/basics/concepts.h>
+#include <MyCEL/types/static_map.hpp>
 
-    template <EnumType NewValue>
-    using append_enum_value = enum_tuple<EnumType, EnumVals..., NewValue>;
 
-    template <EnumType NewValue>
-    using prepend_enum_value = enum_tuple<EnumType, NewValue, EnumVals...>;
-};
-
-template <std::size_t Index, typename EnumTuple>
-struct enum_tuple_element
-{
-    using type                        = EnumTuple;
-    static constexpr const auto index = Index;
-};
-
-template <std::size_t Index, typename EnumType, EnumType Head, EnumType... Tail>
-struct enum_tuple_element<Index, enum_tuple<EnumType, Head, Tail...>>
-    : enum_tuple_element<Index - 1, enum_tuple<EnumType, Tail...>>
-{
-    static_assert(std::is_enum_v<EnumType>);
-};
-
-template <typename EnumType, EnumType Head, EnumType... Tail>
-struct enum_tuple_element<0, enum_tuple<EnumType, Head, Tail...>>
-{
-    using enum_type                            = EnumType;
-    static constexpr const EnumType enum_value = Head;
-};
-
-template <std::size_t Index = 0, typename EnumTuple, typename EnumTuple::enum_type Value>
-constexpr decltype(auto) enum_tuple_index_impl()
-{
-    if constexpr (enum_tuple_element<Index, EnumTuple>::enum_value == Value)
-        return Index;
-    else
-        return enum_tuple_index_impl<Index + 1, EnumTuple, Value>();
-}
-
-template <typename EnumTuple, typename EnumTuple::enum_type Value>
-constexpr auto enum_tuple_index()
-{
-    return enum_tuple_index_impl<0, EnumTuple, Value>();
-}
-
-template <typename EnumType, std::size_t EnumCount, std::size_t index, typename EnumTuple>
-constexpr decltype(auto) make_enum_tuple_impl()
-{
-    if constexpr (index < EnumCount) {
-        using NewTuple = typename EnumTuple::template append_enum_value<magic_enum::enum_value<EnumType>(index)>;
-        return make_enum_tuple_impl<EnumType, EnumCount, index + 1, NewTuple>();
-    }
-    else
-        return EnumTuple{};
-}
-
-template <typename EnumType>
-constexpr decltype(auto) make_enum_tuple()
-{
-    return make_enum_tuple_impl<EnumType, magic_enum::enum_count<EnumType>(), 1,
-                                enum_tuple<EnumType, magic_enum::enum_value<EnumType>(0)>>();
-}
-
-struct enum_switch
-{
-    template <std::size_t switch_size, typename EnumTuple, typename DefaultCaseFunctor,
-              template <typename EnumTuple::enum_type> typename EnumFunctor, typename... Args>
-    static constexpr decltype(auto) switch_impl(typename EnumTuple::enum_type value, Args... args)
-    {
-        constexpr const auto enum_startindex = EnumTuple::count - switch_size;
-        if constexpr (switch_size >= 4) {
-            switch (value) {
-            case enum_tuple_element<enum_startindex, EnumTuple>::enum_value:
-                return EnumFunctor<enum_tuple_element<enum_startindex, EnumTuple>::enum_value>{}(
-                    std::forward<Args>(args)...);
-            case enum_tuple_element<enum_startindex + 1, EnumTuple>::enum_value:
-                return EnumFunctor<enum_tuple_element<enum_startindex + 1, EnumTuple>::enum_value>{}(
-                    std::forward<Args>(args)...);
-            case enum_tuple_element<enum_startindex + 2, EnumTuple>::enum_value:
-                return EnumFunctor<enum_tuple_element<enum_startindex + 2, EnumTuple>::enum_value>{}(
-                    std::forward<Args>(args)...);
-            case enum_tuple_element<enum_startindex + 3, EnumTuple>::enum_value:
-                return EnumFunctor<enum_tuple_element<enum_startindex + 3, EnumTuple>::enum_value>{}(
-                    std::forward<Args>(args)...);
-            default:
-                return switch_impl<switch_size - 4, EnumTuple, DefaultCaseFunctor, EnumFunctor, Args...>(
-                    value, std::forward<Args>(args)...);
-            }
-        }
-        else if constexpr (switch_size >= 2) {
-            switch (value) {
-            case enum_tuple_element<enum_startindex, EnumTuple>::enum_value:
-                return EnumFunctor<enum_tuple_element<enum_startindex, EnumTuple>::enum_value>{}(
-                    std::forward<Args>(args)...);
-            case enum_tuple_element<enum_startindex + 1, EnumTuple>::enum_value:
-                return EnumFunctor<enum_tuple_element<enum_startindex + 1, EnumTuple>::enum_value>{}(
-                    std::forward<Args>(args)...);
-            default:
-                return switch_impl<switch_size - 2, EnumTuple, DefaultCaseFunctor, EnumFunctor, Args...>(
-                    value, std::forward<Args>(args)...);
-            }
-        }
-        else if constexpr (switch_size == 1) {
-            switch (value) {
-            case enum_tuple_element<enum_startindex, EnumTuple>::enum_value:
-                return EnumFunctor<enum_tuple_element<enum_startindex, EnumTuple>::enum_value>{}(
-                    std::forward<Args>(args)...);
-            default:
-                return switch_impl<switch_size - 1, EnumTuple, DefaultCaseFunctor, EnumFunctor, Args...>(
-                    value, std::forward<Args>(args)...);
-            }
-        }
-        else {
-            static_assert(switch_size == 0);
-            return DefaultCaseFunctor{}(std::forward<Args>(args)...);
-        }
-    };
-
-    template <typename EnumType, typename DefaultCaseFunctor, template <EnumType> typename EnumFunctor,
-              typename... Args>
-    static constexpr decltype(auto) run(EnumType Value, Args &&...args)
-    {
-        static_assert(std::is_enum_v<EnumType>);
-        using EnumTuple = decltype(make_enum_tuple<EnumType>());
-        return switch_impl<EnumTuple::count, EnumTuple, DefaultCaseFunctor, EnumFunctor, Args...>(
-            Value, std::forward<Args>(args)...);
-    };
-};
+using namespace MyCEL;
 
 // Helper concept
 ///////////////////////////////////////////////////////////////////
-template <typename T>
-struct is_std_array : std::false_type
-{
-};
 
-template <typename T, std::size_t N>
-struct is_std_array<std::array<T, N>> : std::true_type
-{
-};
-
-template <typename T>
-concept std_array = is_std_array<std::decay_t<T>>::value;
 ///////////////////////////////////////////////////////////////////
 
 template <const auto &, template <auto...> typename, typename>
@@ -231,7 +53,7 @@ struct apply_array_on_type<Input, Output_t>
 {
     using result_type = typename unfold_array_on_type<Input, Output_t, std::make_index_sequence<N>>::result_type;
 };
-template <std_array auto Input,
+template <MyCEL::std_array auto Input,
           template <typename std::remove_reference_t<decltype(Input)>::value_type...> typename Output_t>
 using apply_array_on_type_result_t = typename apply_array_on_type<Input, Output_t>::result_type;
 
@@ -264,14 +86,6 @@ constexpr auto map_values_to_types()
 template <template <typename...> typename Output, const auto &Input,
           template <std::remove_cvref_t<decltype(get<0>(Input))>...> typename Mapping>
 using map_values_to_types_t = decltype(map_values_to_types_impl<Output, Input, Mapping>((std::make_index_sequence<size(Input)>())));
-
-template <const auto &Input, template <std::remove_cvref_t<decltype(get<0>(Input))>...> typename Output, std::size_t... Is>
-constexpr auto apply_nttp_impl(std::index_sequence<Is...>) -> Output<get<Is>(Input)...>;
-template <const auto &Input, template <std::remove_cvref_t<decltype(get<0>(Input))>...> typename Output>
-constexpr auto apply_nttp() -> decltype(apply_nttp_impl<Input,Output>(std::make_index_sequence<size(Input)>()));
-template <const auto &Input, template <std::remove_cvref_t<decltype(get<0>(Input))>...> typename Output>
-using apply_nttp_t = decltype(apply_nttp_impl<Input, Output>(std::make_index_sequence<size(Input)>()));
-
 
 enum class testenum
 {
@@ -356,6 +170,8 @@ constexpr const test_enum_map testenum_map{{{{testenum::value1, "value1"sv},
                                              {testenum::value5, "value5"sv},
                                              {testenum::value6, "value6"sv},
                                              {testenum::value7, "value7"sv}}}};
+
+static_assert(testenum_map.get_key_array()[3]==testenum::value4);
 
 using inv_test_enum_map = static_map<std::string_view, testenum, magic_enum::enum_count<testenum>()>;
 constexpr const inv_test_enum_map testenum_mapswitched{testenum_map.switch_key_value()};
